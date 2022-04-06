2c20b79e586fa5cee9680d9bd711f0f018daeec8
ArvindPanguluri25 <74370692+ArvindPanguluri25@users.noreply.github.com>
2022-04-05 22:02:49 -0400

    Update event_subscribers.dart
    event_subscribers changed

--- lib/models/event_subscribers.dart
+++ lib/models/event_subscribers.dart
@@ -1,1 +1,18 @@
 
+class EventSubscribersData {
+
+  final String? event_id;
+  final String? user_id;
+
+  EventSubscribersData({this.event_id, this.user_id});
+
+  factory EventSubscribersData.initialData() {
+    return EventSubscribersData(
+      event_id: '',
+      user_id: ''
+    );
+  }
+
+}
+
+
