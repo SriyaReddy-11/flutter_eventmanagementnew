d6b2729917813594c0ff6b124058bf3dc5ef2bbc
praveenk111 <58185797+praveenk111@users.noreply.github.com>
2022-04-05 23:29:26 -0400

    data for events

--- lib/models/eventsData.dart
+++ lib/models/eventsData.dart
@@ -1,1 +1,33 @@
 
+
+class EventData{
+
+  final String? event_id;
+  final String? event_name;
+  final String? event_description;
+  final String? photo_url;
+  final String? event_category;
+  final String? lat;
+  final String? lang;
+
+  final DateTime? event_date ;
+
+
+  EventData({this.event_id, this.event_name, this.event_description, this.photo_url, this.event_category, this.lat, this.lang, this.event_date});
+
+
+  factory EventData.initialData() {
+    return EventData(
+      event_id: '',
+      event_name: '',
+      event_description: '',
+      photo_url: '',
+      event_category:  '',
+      lang: '',
+      lat: '',
+      event_date: null,
+    );
+  }
+
+
+}
