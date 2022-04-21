223675f5aba38d2832f2e52164da73b5ead45aca
arjun3333 <56396879+arjun3333@users.noreply.github.com>
2022-04-21 00:08:01 -0400

    updated subscribe evnt

--- lib/screens/subscribe_success.dart
+++ lib/screens/subscribe_success.dart
@@ -1,51 +1,53 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:eventsmangers2/models/event_subscribers.dart';
-import 'package:eventsmangers2/models/eventsData.dart';
-import 'package:eventsmangers2/models/user.dart';
-import 'package:eventsmangers2/models/userData.dart';
-import 'package:eventsmangers2/screens/event_details.dart';
-import 'package:eventsmangers2/services/databases.dart';
-import 'package:eventsmangers2/utils/event_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-
-class MyEventsLists extends StatefulWidget {
-  const MyEventsLists({Key? key}) : super(key: key);
+class SubscribeSuccess extends StatefulWidget {
+  const SubscribeSuccess({Key? key}) : super(key: key);
 
   @override
-  _MyEventsListsState createState() => _MyEventsListsState();
+  State<SubscribeSuccess> createState() => _SubscribeSuccessState();
 }
 
-class _MyEventsListsState extends State<MyEventsLists> {
+class _SubscribeSuccessState extends State<SubscribeSuccess> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: Text("Successfully subscribed"),
+      ),
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.check_circle_outline, color: Colors.green, size: 100,),
+            SizedBox(height: 30,),
+            Container(
+              child: Text("Successfully subscribed", style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                side: BorderSide(width: 3, color: Colors.brown),
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0.0)),
+                padding: const EdgeInsets.all(0),
+              ),
+              onPressed: () async {
+                Navigator.of(context).pop();
+              },
 
-    // final users= Provider.of<List<UserData>>(context);
-    final user = Provider.of<MyUser?>(context);
-    final my_events= Provider.of<List<EventSubscribersData>>(context);
-
-    return ListView.builder(
-      itemBuilder:(context, index){
-        print(my_events[index].rejected);
-        print(my_events[index].accepted);
-        return MultiProvider(
-          providers: [
-            StreamProvider<List<EventData>>.value(
-              value:DatabaseService().eventsById(my_events[index].event_id!),
-              initialData: [],),
-            StreamProvider<List<UserData>>.value(
-              initialData: [],
-              value: DatabaseService().usersById(user!.uid),
+              child: Text(
+                "Back",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              ),
             ),
           ],
-          child: EventsTile(accepted: my_events[index].accepted!, id: my_events[index].subscription_id!, rejected:  my_events[index].rejected!,),
-        );
-      },
-      itemCount: my_events.length,
-      shrinkWrap: true,
-      padding: EdgeInsets.all(5),
-      scrollDirection: Axis.vertical,
+        ),
+      ),
     );
   }
 }
